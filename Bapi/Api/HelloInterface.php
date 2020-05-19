bf3bc190cc607eaffc13c2eeaaaa12d0f9294ee3
Diego <diegoruys@gmail.com>
2020-05-19 09:45:36 -0300

    Para funcionar é obrigatório o bloco de documentação

--- Bapi/Api/HelloInterface.php
+++ Bapi/Api/HelloInterface.php
@@ -4,7 +4,14 @@ namespace Diego\Bapi\Api;
  
 interface HelloInterface
 {
-   
+    /**
+     * Returns greeting message to user
+     *
+     * @api
+     * @param string $name Users name.
+     * @return string Greeting message with users name.
+     */
+    
     public function name($name);
 }
 
--- Bapi/Model/Hello.php
+++ Bapi/Model/Hello.php
@@ -5,7 +5,14 @@ use Diego\Bapi\Api\HelloInterface;
  
 class Hello implements HelloInterface
 {
- 
+    /**
+     * Returns greeting message to user
+     *
+     * @api
+     * @param string $name Users name.
+     * @return string Greeting message with users name.
+     */
+    
     public function name($name) {
         return "Hello, " . $name;
     }
